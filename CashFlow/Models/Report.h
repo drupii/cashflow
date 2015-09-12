b4fbee259dca148e07d909300c82d8c60088dc98
Takuya Murakami <tmurakam@tmurakam.org>
2015-09-12 17:31:21 +0900

    Report.h: nullable, nonnull

--- CashFlow/Models/Report.h
+++ CashFlow/Models/Report.h
@@ -33,9 +33,9 @@
 /** レポート種別 (REPORT_XXX) */
 @property(nonatomic,assign) NSInteger type;
 /** 期間毎の ReportEntry の配列 */
-@property(nonatomic,strong) NSMutableArray<ReportEntry *> *reportEntries;
+@property(nonatomic,strong,nonnull) NSMutableArray<ReportEntry *> *reportEntries;
 
-- (void)generate:(NSInteger)type asset:(Asset *)asset;
+- (void)generate:(NSInteger)type asset:(nullable Asset *)asset;
 @property (nonatomic, getter=getMaxAbsValue, readonly) double maxAbsValue;
 
 @end
@@ -46,10 +46,10 @@
 @interface ReportEntry : NSObject
 
 /** 期間開始日 */
-@property(nonatomic,strong,readonly) NSDate *start;
+@property(nonatomic,strong,readonly,nonnull) NSDate *start;
 
 /** 期間終了日 */
-@property(nonatomic,strong,readonly) NSDate *end;
+@property(nonatomic,strong,readonly,nonnull) NSDate *end;
 
 /** 期間内の総収入 */
 @property(nonatomic,assign,readonly) double totalIncome;
@@ -64,15 +64,15 @@
 @property(nonatomic,assign,readonly) double maxOutgo;
 
 /** カテゴリ毎の収入レポート */
-@property(nonatomic,strong,readonly) NSMutableArray<CatReport *> *incomeCatReports;
+@property(nonatomic,strong,readonly,nonnull) NSMutableArray<CatReport *> *incomeCatReports;
 
 /** カテゴリ毎の支出レポート */
-@property(nonatomic,strong,readonly) NSMutableArray<CatReport *> *outgoCatReports;
+@property(nonatomic,strong,readonly,nonnull) NSMutableArray<CatReport *> *outgoCatReports;
 
-- (instancetype)init NS_UNAVAILABLE;
-- (instancetype)initWithAsset:(NSInteger)assetKey start:(NSDate *)start end:(NSDate *)end NS_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)init NS_UNAVAILABLE;
+- (nonnull instancetype)initWithAsset:(NSInteger)assetKey start:(nonnull NSDate *)start end:(nonnull NSDate *)end NS_DESIGNATED_INITIALIZER;
 
-- (BOOL)addTransaction:(Transaction*)t;
+- (BOOL)addTransaction:(nonnull Transaction*)t;
 - (void)sortAndTotalUp;
 
 @end
@@ -94,12 +94,12 @@
 @property(nonatomic,readonly) double sum;
 
 /** 本カテゴリに含まれる Transaction 一覧 */
-@property(nonatomic,strong,readonly) NSMutableArray<Transaction *> *transactions;
+@property(nonatomic,strong,readonly,nonnull) NSMutableArray<Transaction *> *transactions;
 
-- (instancetype)init NS_UNAVAILABLE;
-- (instancetype)initWithCategory:(NSInteger)category withAsset:(NSInteger)assetKey NS_DESIGNATED_INITIALIZER;
-- (void)addTransaction:(Transaction*)t;
+- (nonnull instancetype)init NS_UNAVAILABLE;
+- (nonnull instancetype)initWithCategory:(NSInteger)category withAsset:(NSInteger)assetKey NS_DESIGNATED_INITIALIZER;
+- (void)addTransaction:(nonnull Transaction*)t;
 
-@property (nonatomic, readonly, copy) NSString *title;
+@property (nonatomic, readonly, copy, nonnull) NSString *title;
 
 @end
