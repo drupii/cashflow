c622bb5d3f2bb8fe9c7e65826e2cafc4aef45300
Takuya Murakami <tmurakam@tmurakam.org>
2015-09-22 18:12:50 +0900

    update DataModel.swift

--- CashFlow/Models/DataModel.swift
+++ CashFlow/Models/DataModel.swift
@@ -11,17 +11,11 @@ protocol DataModelDelegate {
 }
 
 class DataModel : NSObject {
-    var _journal: Journal
-    var _ledger: Ledger
-    var _categories: Categories
+    var journal: Journal
+    var ledger: Ledger
+    var categories: Categories
     
     private(set) var isLoadDone: Bool
-
-    //@property (nonatomic, getter=getBackupSqlPath, readonly, copy, nonnull) NSString *backupSqlPath;
-
-    //@property (nonatomic, getter=isModifiedAfterSync, readonly) BOOL modifiedAfterSync;
-
-    private(set) var _lastModificationDateOfDatabase: NSDate?
 
     private(set) var _delegate: DataModelDelegate?
 
@@ -49,24 +43,24 @@ class DataModel : NSObject {
     }
 
     override init() {
-        _journal = Journal()
-        _ledger = Ledger()
-        _categories = Categories()
+        journal = Journal()
+        ledger = Ledger()
+        categories = Categories()
         self.isLoadDone = false
         
         super.init()
     }
 
-    static func journal() -> Journal {
-        return DataModel.instance()._journal
+    static func getJournal() -> Journal {
+        return DataModel.instance().journal
     }
 
-    static func ledger() -> Ledger {
-        return DataModel.instance()._ledger
+    static func getLedger() -> Ledger {
+        return DataModel.instance().ledger
     }
 
-    static func categories() -> Categories {
-        return DataModel.instance()._categories
+    static func getCategories() -> Categories {
+        return DataModel.instance().categories
     }
 
     func startLoad(delegate: DataModelDelegate) {
@@ -106,31 +100,23 @@ class DataModel : NSObject {
         DescLRUManager.migrate()
 	
         // Load all transactions
-        _journal.reload()
+        journal.reload()
 
         // Load ledger
-        _ledger.load()
-        _ledger.rebuild()
+        ledger.load()
+        ledger.rebuild()
 
         // Load categories
-        _categories.reload()
+        categories.reload()
     }
     
-    ////////////////////////////////////////////////////////////////////////////
-    // Utility
-
-// 摘要からカテゴリを推定する
-//
-// note: 本メソッドは Asset ではなく DataModel についているべき
-//
-- (NSInteger)categoryWithDescription:(NSString *)desc
-{
-    Transaction *t = [Transaction find_by_description:desc cond:@"ORDER BY date DESC"];
-
-    if (t == nil) {
-        return -1;
+    // 摘要からカテゴリを推定する
+    func categoryWithDescription(desc: String) -> Int {
+        let t = Transaction.find_by_description(desc, cond:"ORDER BY date DESC")
+        if (t == nil) {
+            return -1
+        }
+        return t!.category
     }
-    return t.category;
-}
 }
 
