c8b8af2d7d440b2abba4e3bbe13636ca985942ed
Takuya Murakami <tmurakam@tmurakam.org>
2015-09-27 22:09:36 +0900

    refactor test: PinControllerTests -> Swift

---
+++ CashFlowTests/ApplicationTests/PinControllerTests.swift
@@ -0,0 +1,90 @@
+import UIKit
+import XCTest
+
+@testable import CashFlow
+
+class PinControllerTest : ViewControllerWithNavBarTestCase {
+    var pinController: PinController!
+    
+    override func createViewController() -> UIViewController {
+        // AssetListView は storyboard から生成する
+        let sb = UIStoryboard(name: "AssetListView", bundle: nil)
+
+        // 最上位は navigation controller なので、ここから AssetListViewController を取り出す
+        let nv = sb.instantiateInitialViewController() as! UINavigationController
+        let av = nv.topViewController!
+
+        // 重要: loadView を実行する
+        av.performSelectorOnMainThread(Selector("loadView"), withObject: nil, waitUntilDone: true)
+        return av
+    }
+
+    func assetListViewController() -> AssetListViewController? {
+        return self.viewController as? AssetListViewController
+    }
+
+    // MARK: - UIViewControllerTest methods
+
+    override func viewControllerName() -> String {
+        return "AssetListViewController"
+    }
+
+    override func viewControllerNibName() -> String {
+        return "AssetListView"
+    }
+
+    func hasNavigationController() -> Bool {
+        return true
+    }
+    
+    // MARK: -
+
+    
+    override func setUp() {
+        super.setUp()
+
+        PinController._deleteSingleton()
+        self.pinController = PinController.sharedController()
+
+        //[self.vc viewDidLoad]; // ###
+        //[self.vc viewWillAppear:YES]; // ###
+    }
+
+    override func tearDown() {
+        //[vc viewWillDisappear:YES];
+        super.tearDown()
+    }
+
+    func testNoPin() {
+        let saved = self.pinController
+    
+        self.pinController.pin = nil
+        self.pinController.firstPinCheck(self.viewController)
+        // この時点で、Pin チェック完了したため、PinController の singleton は削除されているはず
+    
+        let new = PinController.sharedController()
+        XCTAssert(new != saved)
+    
+        // modal view がでていないことを確認する
+        //AssertNil(self.vc.modalViewController);
+        XCTAssertNil(self.viewController.presentedViewController)
+    }
+
+    func testHasPin() {
+        let saved = self.pinController
+    
+        self.pinController.pin = "1234"
+        self.pinController.firstPinCheck(self.viewController)
+
+        // Pin があるため、この時点ではまだ終了していないはず
+        let new = PinController.sharedController()
+        XCTAssert(new == saved);
+    
+        // modal view が出ていることを確認する
+        let nv = self.viewController.presentedViewController as! UINavigationController
+        XCTAssertNotNil(nv)
+        
+        let pv = nv.viewControllers[0] as? PinViewController
+        XCTAssertNotNil(pv)
+    }
+}
