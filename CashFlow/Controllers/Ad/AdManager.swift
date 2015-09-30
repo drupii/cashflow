8bede28d346c528ab3bc2c3744ce2a5df2e730ee
Takuya Murakami <tmurakam@tmurakam.org>
2015-09-30 22:28:24 +0900

    refactor: AdView 分割

--- CashFlow/Controllers/Ad/AdManager.swift
+++ CashFlow/Controllers/Ad/AdManager.swift
@@ -7,41 +7,6 @@
 import UIKit
 
 import GoogleMobileAds
-  
-/**
- *  AdMob 表示用ラッパクラス。xxxBannerView を継承。
- */
-class AdView : DFPBannerView, GADBannerViewDelegate {
-    override init(adSize: GADAdSize) {
-        super.init(adSize: adSize)
-        self.delegate = self
-    }
-    
-    convenience init() {
-        var adSize: GADAdSize
-    
-        if isIpad() {
-            // 320 x 50 固定。こうしないと在庫でない模様
-            adSize = kGADAdSizeBanner
-            
-            //adSize = kGADAdSizeSmartBannerPortrait
-        } else {
-            adSize = kGADAdSizeBanner
-
-            // option 2
-            //adSize = GADAdSizeFullWidthPortraitWithHeight(50);
-            
-            // option 3 : SmartBanner
-            //adSize = kGADAdSizeSmartBannerPortrait
-        }
-        self.init(adSize: adSize)
-        //print(self.frame.size)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
 
 /**
  * 広告マネージャ
